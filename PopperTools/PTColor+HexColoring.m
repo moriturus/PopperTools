047815a3b942d6dac53360b7ed102052e1e1ba52
Henrique Sasaki Yuya <es.moriturus@icloud.com>
2014-02-22 22:50:29 +0900

    write testing

--- PopperTools/PTColor+HexColoring.m
+++ PopperTools/PTColor+HexColoring.m
@@ -32,10 +32,10 @@
 
 + (PTColor*)colorWithHex:(uint32_t)hex
 {
-    CGFloat red = ((hex & 0xff000000) >> 24) / 255.0f;
+    CGFloat red   = ((hex & 0xff000000) >> 24) / 255.0f;
     CGFloat green = ((hex & 0x00ff0000) >> 16) / 255.0f;
-    CGFloat blue = ((hex & 0x0000ff00) >> 8) / 255.0f;
-    CGFloat alpha = (hex & 0x000000ff) / 255.0f;
+    CGFloat blue  = ((hex & 0x0000ff00) >> 8) / 255.0f;
+    CGFloat alpha = ( hex & 0x000000ff) / 255.0f;
     
 #if TARGET_OS_IPHONE == 1
     
--- PopperToolsTests/PopperToolsTests.m
+++ PopperToolsTests/PopperToolsTests.m
@@ -9,6 +9,32 @@
 #import <XCTest/XCTest.h>
 #import "PopperTools.h"
 
+@interface PTSwizzleClass : NSObject
+
+@end
+
+@implementation PTSwizzleClass
+
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self) {
+        
+        [[self class] swizzleMethodFrom:@selector(description) to:@selector(sayHello)];
+        
+    }
+    
+    return self;
+}
+
+- (NSString*)sayHello
+{
+    return @"hello";
+}
+
+@end
+
 @interface PopperToolsTests : XCTestCase
 
 @end
@@ -52,5 +78,166 @@
     
     XCTAssertEqual(0.0, [date timeIntervalSince1970], @"%s failed",__PRETTY_FUNCTION__);
 }
+
+- (void)testNSDictionary_sortedKeyEnumeratorWithSortOption_ascending
+{
+    NSDictionary* dictionary = @{
+                                 @"b":@"0",
+                                 @"a":@"1"
+                                 };
+    
+    NSEnumerator* enumerator = [dictionary sortedKeyEnumeratorWithSortOption:PTSortOptionAscending];
+    NSArray* keys = [enumerator allObjects];
+    
+    XCTAssertEqual(@"a", keys[0], @"keys[0] => %@",keys[0]);
+}
+
+- (void)testNSDictionary_sortedKeyEnumeratorWithSortOption_descending
+{
+    NSDictionary* dictionary = @{
+                                 @"a":@"0",
+                                 @"b":@"1"
+                                 };
+    
+    NSEnumerator* enumerator = [dictionary sortedKeyEnumeratorWithSortOption:PTSortOptionDescending];
+    NSArray* keys = [enumerator allObjects];
+    
+    XCTAssertEqual(@"b", keys[0], @"keys[0] => %@",keys[0]);
+}
+
+- (void)testNSMutableArray_randomize
+{
+    NSArray* originalArray = @[@0,@1,@2,@3,@4];
+    NSMutableArray* array = [NSMutableArray arrayWithArray:originalArray];
+    [array randomize];
+    
+    XCTAssertFalse([array isEqualToArray:originalArray], @"%s failed",__PRETTY_FUNCTION__);
+}
+
+- (void)testNSMutableArray_randomizeWithCount_5
+{
+    NSArray* originalArray = @[@0,@1,@2,@3,@4,@5,@6,@7,@8,@9];
+    NSMutableArray* array = [NSMutableArray arrayWithArray:originalArray];
+    [array randomizeWithCount:5];
+    
+    XCTAssertFalse([array isEqualToArray:originalArray], @"%s failed",__PRETTY_FUNCTION__);
+}
+
+- (void)testNSMutableArray_randomizeWithCount_6
+{
+    NSArray* originalArray = @[@0,@1,@2,@3,@4];
+    NSMutableArray* array = [NSMutableArray arrayWithArray:originalArray];
+    
+    XCTAssertThrows([array randomizeWithCount:6], @"%s failed",__PRETTY_FUNCTION__);
+}
+
+- (void)testNSObject_swizzleMethodFrom_to
+{
+    NSString* testString = @"hello";
+    PTSwizzleClass* testClass = [[PTSwizzleClass alloc] init];
+    
+    XCTAssertEqual(testString, [testClass description], @"%@ != %@",testString,[testClass description]);
+}
+
+- (void)testNSObject_isNSNull_true
+{
+    id object = [NSNull null];
+    
+    XCTAssertTrue([object isNSNull], @"object is not NSNull");
+}
+
+- (void)testNSObject_isNSNull_false
+{
+    id object = [[NSObject alloc] init];
+    
+    XCTAssertFalse([object isNSNull], @"object is NSNull");
+}
+
+- (void)testNSObject_referenceCount_1
+{
+    id object = [[NSObject alloc] init];
+    
+    XCTAssertEqual(1l, [object referenceCount], @"reference count is %ld",[object referenceCount]);
+}
+
+- (void)testNSObject_referenceCount_2
+{
+    id object = [[NSObject alloc] init];
+    NSArray* array = @[object];
+    
+    XCTAssertEqual(2l, [object referenceCount], @"reference count is %ld",[object referenceCount]);
+    
+    array = nil;
+}
+
+- (void)testNSURL_appLibraryDirectoryURL
+{
+    NSString* testString = @"Library";
+    NSURL* url = [NSURL appLibraryDirectoryURL];
+    
+    XCTAssertTrue([testString isEqualToString:[url lastPathComponent]], @"last path component is not Library, %@",[url lastPathComponent]);
+}
+
+- (void)testNSURL_appLibraryCacheDirectoryURL
+{
+    NSString* testString = @"Caches";
+    NSURL* url = [NSURL appLibraryCacheDirectoryURL];
+    
+    XCTAssertTrue([testString isEqualToString:[url lastPathComponent]], @"last path component is not Caches, %@",[url lastPathComponent]);
+}
+
+- (void)testNSURL_userDocumentsDirectoryURL
+{
+    NSString* testString = @"Documents";
+    NSURL* url = [NSURL userDocumentsDirectoryURL];
+    
+    XCTAssertTrue([testString isEqualToString:[url lastPathComponent]], @"last path component is not Documents, %@",[url lastPathComponent]);
+}
+
+- (void)testNSURL_downloadDirectoryURL
+{
+    NSString* testString = @"download";
+    NSURL* url = [NSURL downloadDirectoryURL];
+    
+    XCTAssertTrue([testString isEqualToString:[url lastPathComponent]], @"last path component is not download, %@",[url lastPathComponent]);
+}
+
+/*
+
+- (void)testPTColor_colorWithHex
+{
+    UIColor* testColor = [UIColor whiteColor];
+    UIColor* color = [UIColor colorWithHex:0xffffffff];
+    
+    CGFloat testRed = 0.0;
+    CGFloat testGreen = 0.0;
+    CGFloat testBlue = 0.0;
+    CGFloat testAlpha = 0.0;
+    
+    BOOL testConverted = [testColor getRed:&testRed green:&testGreen blue:&testBlue alpha:&testAlpha];
+    
+    XCTAssertTrue(testConverted, @"testColor converting failed");
+    
+    CGFloat red = 0.0;
+    CGFloat green = 0.0;
+    CGFloat blue = 0.0;
+    CGFloat alpha = 0.0;
+    
+    BOOL converted = [color getRed:&red green:&green blue:&blue alpha:&alpha];
+    
+    XCTAssertTrue(converted, @"color converting failed");
+    
+    XCTAssertEqual(testRed, red, @"%f != %f",testRed,red);
+    XCTAssertEqual(testGreen, green, @"%f != %f",testGreen,green);
+    XCTAssertEqual(testBlue, blue, @"%f != %f",testBlue,blue);
+    XCTAssertEqual(testAlpha, alpha, @"%f != %f",testAlpha,alpha);
+    
+    
+}
+ 
+ */
+
+
+
 
 @end
